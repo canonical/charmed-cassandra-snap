eea9e04fd9e37ab784576811cfbc965661b436fc
Vladyslav Yurchenko <silvia.homam@gmail.com>
2025-06-25 14:49:05 +0300

    [MISC] Handle MAX_HEAP_SIZE and HEAP_NEWSIZE via snap configuration

--- snap/local/opt/cassandra/bin/cassandra-utils.sh
+++ snap/local/opt/cassandra/bin/cassandra-utils.sh
@@ -29,3 +29,18 @@ set_heap_new_size_mb()
 {
 	snapctl set heap-new-size-mb="$1"
 }
+
+detect_running_daemon() {
+    daemon_active=$(snapctl services charmed-cassandra.daemon | awk '/charmed-cassandra.daemon/ { print $3 }')
+    mgmt_active=$(snapctl services charmed-cassandra.mgmt-server | awk '/charmed-cassandra.mgmt-server/ { print $3 }')
+
+    if [ "$daemon_active" = "active" ] && [ "$mgmt_active" = "active" ]; then
+        echo "Both daemons are running"
+    elif [ "$daemon_active" = "active" ]; then
+        echo "daemon"
+    elif [ "$mgmt_active" = "active" ]; then
+        echo "mgmt-server"
+    else
+        echo "No daemons are running"
+    fi
+}
