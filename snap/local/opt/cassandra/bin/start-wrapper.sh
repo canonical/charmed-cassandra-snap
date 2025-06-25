bd355707f0c8b2649acdd825c3a3ad76181be385
Vladyslav Yurchenko <silvia.homam@gmail.com>
2025-06-25 15:35:24 +0300

    [MISC] Handle MAX_HEAP_SIZE and HEAP_NEWSIZE via snap env

--- snap/local/opt/cassandra/bin/start-wrapper.sh
+++ snap/local/opt/cassandra/bin/start-wrapper.sh
@@ -3,6 +3,23 @@
 set -eu
 
 source "${SNAP}"/opt/shared/bin/snap-interfaces.sh
+source "${SNAP}"/opt/cassandra/bin/cassandra-utils.sh
+
+# === Heap size logic ===
+max_heap_size="$(max_heap_size_mb)"
+heap_new_size="$(heap_new_size_mb)"
+
+if ! echo "$max_heap_size" | grep -Eq '^[0-9]+$'; then
+    max_heap_size=""
+fi
+
+if ! echo "$heap_new_size" | grep -Eq '^[0-9]+$'; then
+    heap_new_size=""
+fi
+
+env_vars=()
+[ -n "$max_heap_size" ] && env_vars+=(MAX_HEAP_SIZE="${max_heap_size}M")
+[ -n "$heap_new_size" ] && env_vars+=(HEAP_NEWSIZE="${heap_new_size}M")
 
 function start_cassandra() {
   exit_if_missing_perm "system-observe"
@@ -11,7 +28,8 @@ function start_cassandra() {
 
   echo "Starting Cassandra..."
 
-  "${SNAP}"/usr/bin/setpriv \
+  env "${env_vars[@]}" \
+    "${SNAP}"/usr/bin/setpriv \
     --clear-groups \
     --reuid _daemon_ \
     --regid _daemon_ -- \
